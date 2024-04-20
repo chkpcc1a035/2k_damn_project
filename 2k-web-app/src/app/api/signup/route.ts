16725c4393e62daeaf79a797def10e9240300a1d
RaymondYKL <91876308+chkpcc1a035@users.noreply.github.com>
2024-04-20 12:45:22 +0800

    Fix signup UI and update form validation in SignUpGrid.tsx

--- 2k-web-app/src/app/api/signup/route.ts
+++ 2k-web-app/src/app/api/signup/route.ts
@@ -1,3 +1,12 @@
 //src/app/api/auth/[...nextauth]/route.ts
 import { handlers } from "@/auth"; // Referring to the auth.ts we just created
-export const { GET, POST } = handlers;
+// export const { GET, POST } = handlers;
+export async function GET(request: Request) {
+  // const cookieStore = cookies()
+  // const token = cookieStore.get('token')
+
+  return new Response("Hello, Next.js!", {
+    status: 200,
+    headers: { Yolo: "hi" },
+  });
+}
--- 2k-web-app/src/components/ui/SignUpGrid.tsx
+++ 2k-web-app/src/components/ui/SignUpGrid.tsx
@@ -1,5 +1,7 @@
+"use client";
 import {
   Button,
+  Card,
   Container,
   Divider,
   Grid,
@@ -11,55 +13,94 @@ import {
   Text,
   TextInput,
 } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import { IconBrandMeta, IconBrandX } from "@tabler/icons-react";
+import { sign } from "crypto";
 import { TbBrandFacebook, TbBrandTwitter } from "react-icons/tb";
 
 export default function SignUpGrid({ isSigned }: { isSigned: boolean }) {
-  return (
-    <SimpleGrid mt={"5vh"} cols={{ base: 1, xs: 2 }}>
-      <Stack align="left">
-        <Text fw={700} size="2rem">
-          Hello!
-        </Text>
-        <Text c="dimmed">Please signup to continue</Text>
-        <TextInput
-          p="lg"
-          variant="unstyled"
-          label="Username"
-          description="Username of Account"
-          placeholder="jasonbourne"
-        />
-        <TextInput
-          p="lg"
-          variant="unstyled"
-          label="Password"
-          description="Credential of Account"
-          placeholder="Your secret password"
-        />
-        <TextInput
-          p="lg"
-          variant="unstyled"
-          label="Confirm Password"
-          description="Re-enter your password"
-          placeholder="Your secret password again"
-        />
-        <Button color="#c8d3eb" variant="filled">
-          Sign Up
-        </Button>
-        {/* 
-          <Divider label="OR" labelPosition="center"></Divider>
-          <Stack align="center">
-            <Text c="dimmed">SignUp with</Text>
-            <Group>
-              <TbBrandFacebook scale="2rem" />
-              <TbBrandTwitter scale={2} />
-              <IconBrandX />
-              <IconBrandMeta />
-            </Group>
-          </Stack> */}
-      </Stack>
+  const signUpForm = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      username: "",
+      password: "",
+      reEnterPassword: "",
+    },
 
-      <Skeleton h={"100%"} visibleFrom="xs" />
-    </SimpleGrid>
+    validate: {
+      username: (value) =>
+        /.{8,}/.test(value)
+          ? null
+          : "The username must be at least 8 characters long",
+      password: (value) =>
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(value)
+          ? null
+          : "The password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number",
+      reEnterPassword: (value, values) =>
+        value === values.password ? null : "Passwords do not match",
+    },
+  });
+  return (
+    <form
+      onSubmit={signUpForm.onSubmit((values) => {
+        console.log("Submitted values:", values);
+        console.log("Password:", values.password);
+        console.log("Re-enter Password:", values.reEnterPassword);
+        if (values.password !== values.reEnterPassword) {
+          console.error("The passwords do not match!");
+        } else {
+          // proceed with the form submission or next steps
+        }
+      })}
+    >
+      <SimpleGrid mt={"5vh"} cols={{ base: 1, xs: 2 }}>
+        <Card withBorder>
+          <Stack align="left">
+            <Text fw={700} size="2rem">
+              Hello!
+            </Text>
+            <Text c="dimmed">Please signup to continue</Text>
+            <TextInput
+              key="username"
+              p="lg"
+              variant="unstyled"
+              label="Username"
+              description="Username of Account"
+              placeholder="jasonbourne"
+              {...signUpForm.getInputProps("username")}
+            />
+            <TextInput
+              p="lg"
+              key="password"
+              variant="unstyled"
+              label="Password"
+              description="Credential of Account"
+              placeholder="Your secret password"
+              {...signUpForm.getInputProps("password")}
+            />
+            <TextInput
+              key="reEnterPassword"
+              p="lg"
+              variant="unstyled"
+              label="Confirm Password"
+              description="Re-enter your password"
+              placeholder="Your secret password again"
+              {...signUpForm.getInputProps("reEnterPassword")}
+            />
+            <Button
+              color="#c8d3eb"
+              variant="filled"
+              type="submit"
+              onClick={() => {
+                console.log("Sign Up button clicked");
+              }}
+            >
+              Sign Up
+            </Button>
+          </Stack>
+        </Card>
+        <Skeleton h={"100%"} visibleFrom="xs" />
+      </SimpleGrid>
+    </form>
   );
 }
